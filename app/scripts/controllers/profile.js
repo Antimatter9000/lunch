e1015cca5d7795a5aa3883454e94c23fd448322e
juicysprings <optimus.sense@gmail.com>
2017-09-29 16:55:48 +0100

    add logo, clean up

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -9,9 +9,5 @@
  */
 angular.module('lunchApp')
   .controller('ProfileCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
--- app/scripts/controllers/scan.js
+++ app/scripts/controllers/scan.js
@@ -11,15 +11,40 @@ angular.module('lunchApp')
   .controller('ScanCtrl', function ($scope) {
     $scope.totalCreditOfItem = 0;
     $scope.scanSuccess = null;
+    $scope.totalKMC = 0;
 
     $scope.submit = function(){
         $scope.totalCreditOfItem = 10;
-
         $scope.scanSuccess = true;
     };
 
-    $scope.reset = function(){
+    $scope.reset = function(){ 
         
     };
-    
+
+    var getRandomNumber = function(){
+        return Math.floor(Math.random() * 100);
+    };
+
+    $scope.item = {
+        name: "7 up",
+        nutrition:[
+            { nutrition: "Calories", amount: getRandomNumber(), KMC: getRandomNumber() },        
+            { nutrition: "Carbohydrate", amount: getRandomNumber(), KMC: getRandomNumber() },
+            { nutrition: "Fat", amount: getRandomNumber(), KMC: getRandomNumber() },
+            { nutrition: "Sodium", amount: getRandomNumber(), KMC:getRandomNumber() },        
+            { nutrition: "Protein", amount: getRandomNumber(), KMC: getRandomNumber() },
+            { nutrition: "Vitamin C", amount: getRandomNumber(), KMC: getRandomNumber() },
+            { nutrition: "Vitamin B", amount: getRandomNumber(), KMC: getRandomNumber() }        
+        ]
+    };
+
+    $scope.getTotalKMC = function(){
+        angular.forEach($scope.item.nutrition, function(object) {
+            $scope.totalKMC += object.KMC;
+          });
+
+        return $scope.totalKMC;
+    };
+
   });
